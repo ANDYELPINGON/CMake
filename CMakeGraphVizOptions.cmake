ba5448783a6baeb91e9351247be1ed2d8273385b
ANDY AXCEL DOMINGUEZ CCORAHUA <154539485+ANDYELPINGON@users.noreply.github.com>
2024-03-10 16:57:47 -0500

    Update CMakeGraphVizOptions.cmake

--- CMakeGraphVizOptions.cmake
+++ CMakeGraphVizOptions.cmake
@@ -1,1 +1,1 @@
-set(GRAPHVIZ_IGNORE_TARGETS "tartest;testSystemTools;testRegistry;testProcess;testIOS;testHashSTL;testFail;testCommandLineArguments;xrtest;LIBCURL;foo")
+set(GRAPHVIZ_IGNORE_TARGETS "tartestinit;testSystemTools;testRegistry;testProcess;testIOS;testHashSTL;testFail;testCommandLineArguments;xrtest;LIBCURL;foo")
